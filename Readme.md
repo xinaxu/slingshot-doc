a7752857bb6d2acb45a538308937561958cb90ba
Xinan Xu <shanexxa@gmail.com>
2021-01-26 07:24:04 +0000

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,10 @@
+This repo is used to generate basic markdown file for Slingshot retrieval doc
+1. Install Ruby
+`sudo apt install ruby`
+2. Install Gems
+`sudo gem install rest-client`
+3. Run script
+`ruby <projectId> <optional descriptionFile>`
+The projectId can be found when clicking into your project detail in the slingshot website, for example
+`https://slingshot.filecoin.io/project/<projectId>`
+You can find example of description file in this repo.
---
+++ slingshot.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/ruby
+require 'rest-client'
+require 'json'
+project = ARGV[0]
+desc = File.read(ARGV[1]).lines.map{|line|line.split(',')}.to_h unless ARGV[1].nil?
+deals = JSON.parse(RestClient.get("https://space-race-slingshot-phase2.s3-us-west-2.amazonaws.com/prod/deals_list_#{project}.json").body)["payload"]
+files = JSON.parse(RestClient.post("https://slingshot.filecoin.io/api/graphql", {operationName: "getDeals", query: "query getDeals($properties: JSON) {getDeals(properties: $properties)}", variables: {properties: {project: project}}}).body)["data"]["getDeals"]
+deals.each do |deal|
+  deal["filename"] = files[deal["deal_id"]]["filename"]
+end
+puts "## To begin"
+puts "First, set a max price that you can tolerate"
+puts "```"
+puts "export MAX_PRICE=`"
+puts "```"
+deals.group_by{|deal| deal["filename"]}.to_a.sort_by{|k,v|k}.each do |filename, deals|
+  puts "## #{filename}"
+  puts "### #{desc[filename]}" if !desc.nil? && desc.has_key?(filename)
+  puts "```"
+  deals.each do |deal|
+    puts "lotus client retrieve --maxPrice $MAX_PRICE --miner #{deal["miner_id"]} #{deal["payload_cid"]}"
+  end
+  puts "```"
+end
