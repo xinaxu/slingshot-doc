eb5f1de648426ddf933c54e49648ef037a033f64
Xinan Xu <shanexxa@gmail.com>
2021-11-13 00:30:49 +0000

    spacecar

---
+++ spacecar.rb
@@ -0,0 +1,102 @@
+#!/usr/bin/ruby
+require 'fileutils'
+require 'pathname'
+require 'slop'
+
+opts = Slop::Options.new
+opts.banner = 'Usage: ./spacecar.rb [options]'
+opts.string '-i', '--input', 'Source directory for the dataset', required: true
+opts.string '-t', '--temp', 'Temporary directory to process the dataset', required: true
+opts.string '-o', '--output', 'Destination directory to store car files', required: true
+opts.int '-s', '--sector', 'Maximum size of a single car file', default: 34359738368 # 32G
+opts.int '-l', '--limit', 'Maximum size of a single file within the car archive', default: 17179869184 # 16G
+opts.int '-x', '--split', 'Size of splitted file if an individual file is more than the maximum limit', default: 17179869184 # 16G
+opts.on '-h', '--help', 'print help' do
+  puts opts
+  exit
+end
+parser = Slop::Parser.new(opts)
+begin
+  options = parser.parse(ARGV)
+rescue
+  puts opts
+  exit
+end
+
+input = options[:input]
+output = options[:output]
+temp = options[:temp]
+sector = options[:sector]
+limit = options[:limit]
+split = options[:split]
+split = limit if split > limit
+
+puts "== Processing the source folder"
+paths = Pathname.glob(File.join(input, '**', '*'))
+puts "Total number of paths: #{paths.length}"
+paths.each do |path|
+  if path.file? && path.size > limit
+    puts "Splitting #{path} into chunks"
+    path.open('r') do |file|
+      until file.eof?
+        id = file.pos / split
+        target = Pathname.new(path.to_s + ".#{"%04d" % id}")
+        puts "Writting to #{target}"
+        target.open('w') do |file_out|
+          file_out << file.read(split)
+        end
+      end
+    end
+    puts "Deleting #{path}"
+    path.delete
+  end
+end
+
+puts "==Generating the destination folder"
+FileUtils.rm_r(temp, force: true)
+FileUtils.mkdir_p(temp)
+dir_size = 0
+id = 0
+output_name = File.join(output, Pathname.new(input).basename.to_s)
+paths = Pathname.glob(File.join(input, '**', '*'))
+system("ipfs repo gc")
+thread = nil
+paths.each do |path|
+  next unless path.file?
+  relative = path.relative_path_from(input)
+  target = Pathname.new(temp).join(relative)
+  FileUtils.mkdir_p(target.dirname.to_s)
+  FileUtils.mv(path.to_s, target.to_s)
+  dir_size += target.size
+  if dir_size >= sector
+    puts "ipfs add -r -p=false --pin=false --nocopy #{temp} > #{output_name}.#{id}.txt"
+    system("ipfs add -r -p=false --pin=false --nocopy #{temp} > #{output_name}.#{id}.txt")
+    cid = File.read("#{output_name}.#{id}.txt").lines[-1].split(' ')[1]
+    puts "ipfs dag export #{cid} > #{output_name}.#{id}.car"
+    system("ipfs dag export #{cid} > #{output_name}.#{id}.car")
+    system("ipfs repo gc")
+    FileUtils.rm_r(temp, force: true)
+    FileUtils.mkdir_p(temp)
+    thread.join unless thread.nil?
+    dir_size = 0
+    id = id + 1
+    thread = Thread.new do
+      system("/mnt/downloads/src/go-graphsplit/graphsplit commP #{output_name}.#{id-1}.car > #{output_name}.#{id-1}.commP")
+    end
+  end
+end
+
+  if dir_size > 0
+    puts "ipfs add -r -p=false --pin=false --nocopy #{temp} > #{output_name}.#{id}.txt"
+    system("ipfs add -r -p=false --pin=false --nocopy #{temp} > #{output_name}.#{id}.txt")
+    cid = File.read("#{output_name}.#{id}.txt").lines[-1].split(' ')[1]
+    puts "ipfs dag export #{cid} > #{output_name}.#{id}.car"
+    system("ipfs dag export #{cid} > #{output_name}.#{id}.car")
+    system("ipfs repo gc")
+    FileUtils.rm_r(temp, force: true)
+    FileUtils.mkdir_p(temp)
+    thread.join unless thread.nil?
+    dir_size = 0
+    id = id + 1
+    system("/mnt/downloads/src/go-graphsplit/graphsplit commP #{output_name}.#{id-1}.car > #{output_name}.#{id-1}.commP")
+  end
